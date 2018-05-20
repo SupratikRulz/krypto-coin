b80f150b7933cf43968eec1de633200f4481a412
Supratik <supratik@fusioncharts.com>
2018-05-20 19:20:52 +0530

    Added isValidChain method to check if the chain is valid.

--- src/blockchain.js
+++ src/blockchain.js
@@ -12,6 +12,21 @@ class Blockchain {
 
         return block;
     }
+
+    isValidChain (chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        }
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+
+            if (block.lastHash !== lastBlock.ownHash || block.ownHash !== Block.getBlockHash(block)) {
+                return false;
+            }
+        }
+        return  true;
+    }
 }
 
 module.exports = Blockchain;
