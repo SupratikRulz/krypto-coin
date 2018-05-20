20bb216abe41753f3bf1e206af01c8c8d9663d22
Supratik <supratik@fusioncharts.com>
2018-05-20 19:35:15 +0530

    Added test for checking validity of an incomming chain

--- src/blockchain.test.js
+++ src/blockchain.test.js
@@ -2,10 +2,12 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain Test', () => {
-    let blockchain;
+    let blockchain,
+        blockchain2;
     
     beforeEach((() => {
         blockchain = new Blockchain();
+        blockchain2 = new Blockchain();
     }));
 
     it('should start with genesis block', () => {
@@ -16,5 +18,21 @@ describe('Blockchain Test', () => {
         const data = 'sample data';
         blockchain.addBlock(data);
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+    });
+
+    it('should validate a valid chain', () => {
+        blockchain2.addBlock('fake data');
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+    });
+
+    it('should invalidate a chain with corrupt genesis block', () => {
+        blockchain2.chain[0].data = 'corrupt data';
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it('should invalidate a corrupt chain', () => {
+        blockchain2.addBlock('good data');
+        blockchain2.chain[1].data = 'data corruption taking place';
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
     });
 });
