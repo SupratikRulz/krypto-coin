b8ba06beba303228c84e76d4dfa8fa2483868417
Supratik <supratik@fusioncharts.com>
2018-05-20 21:03:42 +0530

    Added post request /mine which adds block to the blockchain

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -6,8 +7,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
     res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New block added ${block.toString()}`);
+
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on PORT ${HTTP_PORT}`));
